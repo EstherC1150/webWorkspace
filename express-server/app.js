b0c12ff21e948267c16933a17cb02e2e175b4b95
Daye Choi <daye511@naver.com>
2023-11-28 08:57:18 +0900

    깃이그노어적용쓰

--- express-server/app.js
+++ express-server/app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 
+const getData = async() => {
+    return await fetch('db.json')
+    .then(res => res.json());
+}
+
+let db = getData();
+
 //라우팅 등록
 app.get('/', (req, res)=>{
     res.send('Server Connect');
 });
+
+app.get('/posts', (req, res) => {
+    res.send(db['posts']);
+})
 
 app.listen(3000, ()=>{
     console.log('서버가 실행됩니다.');
