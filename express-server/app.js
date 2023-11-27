2d36c9ea07b61ebe023e377577b2d73ed00ef238
Daye Choi <daye511@naver.com>
2023-11-27 17:45:38 +0900

    231127_다시

---
+++ express-server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+//라우팅 등록
+app.get('/', (req, res)=>{
+    res.send('Server Connect');
+});
+
+app.listen(3000, ()=>{
+    console.log('서버가 실행됩니다.');
+    console.log('http://localhost:3000');
+});
